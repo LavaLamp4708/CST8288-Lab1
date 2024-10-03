62f8ba52778f518c67198e2729d3a5d9ee135c56
Peter Stainforth <stai0029@algonquinlive.com>
2024-10-03 17:04:23 -0400

    Made EmployeeManager singleton.

--- src/employee/EmployeeManager.java
+++ src/employee/EmployeeManager.java
@@ -1,5 +1,16 @@
 package employee;
 
 public class EmployeeManager{
+	private static EmployeeManager emInstance;
 	
+	private EmployeeManager() {
+		
+	}
+	
+	public EmployeeManager getEmployeeManagerInstance() {
+		if(emInstance == null) {
+			emInstance = new EmployeeManager();
+		}
+		return emInstance;
+	}
 }
